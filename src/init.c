58bb6e4e1baf55a447890cc154106e27d11416bc
Moriaan Michiels <moriaan.michiels@gmail.com>
2026-06-09 20:15:22 +0200

    Init check for jcd in path

--- src/init.c
+++ src/init.c
@@ -6,6 +6,11 @@
 void init()
 {
     printf("j() {\n"
+           "  if ! command -v jcd >/dev/null 2>&1; then\n"
+           "    echo \"error: 'jcd' binary not found in PATH.\" >&2\n"
+           "    return 1\n"
+           "  fi\n"
+           "\n"
            "  local target_dir\n"
            "  target_dir=$(jcd cd \"$@\")\n"
            "  if [ $? -eq 0 ] && [ -n \"$target_dir\" ]; then\n"
@@ -13,3 +18,4 @@ void init()
            "  fi\n"
            "}\n");
 }
+
